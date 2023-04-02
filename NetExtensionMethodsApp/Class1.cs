342814a0d7cbd224492603f0a949200b9cf4deae
Ефимов Максим <efimov_m@itstep.academy>
2023-04-02 14:08:20 +0300

    Добавьте файлы проекта.

---
+++ NetExtensionMethodsApp/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExtensionMethodsApp
+{
+    public partial class Employee
+    {
+        public string Phone { set; get; }
+
+        partial void Print()
+        {
+            Console.WriteLine($"{Name} {Age}");
+        }
+    }
+}
---
+++ NetExtensionMethodsApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using NetExtensionMethodsApp;
+
+string s = "Hello world, hello people";
+Console.WriteLine(s.CountChar('l'));
+
+Employee employee = new Employee();
+employee.Name = "Bob";
+employee.Phone = "7 (890) 999-03-87";
+
+var anonimUser = new { FullName = "Joe", BirhtDate = new DateTime(1998, 5, 17) };
+Console.WriteLine(anonimUser.GetType());
+
+var tuple = ("Hello", 35);
+Console.WriteLine(tuple.Item1);
+Console.WriteLine(tuple.Item2);
+Console.WriteLine(tuple.GetType());
+
+int a = 10;
+int b = 20;
+Console.WriteLine($"{a} {b}");
+(a, b) = (b, a);
+Console.WriteLine($"{a} {b}");
+
+PrintEmployee(tuple);
+
+void PrintEmployee((string name, int age) employe)
+{
+    Console.WriteLine($"{employe.name} {employe.age}");
+}
+
+static class StringExtension
+{
+    public static int CountChar(this string str, char symbol)
+    {
+        int count = 0;
+        for(int i = 0; i < str.Length; i++)
+            if (str[i] == symbol)
+                count++;
+        return count;
+    }
+}
+
